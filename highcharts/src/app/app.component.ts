3b42e60810be1c16441778ff9a5529ac443b2711
omarVodiaK <omar.boudfoust@gmail.com>
2020-11-03 00:05:25 +0800

    init

---
+++ highcharts/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'highcharts';
+}
